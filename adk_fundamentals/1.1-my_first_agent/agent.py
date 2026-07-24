a014dc090b9ddae6ca9912d880024b2adc350344
cats-vs-dogs <tchonkov.i@gmail.com>
2026-07-24 14:00:37 +0000

    course 2

--- adk_fundamentals/1.1-my_first_agent/agent.py
+++ adk_fundamentals/1.1-my_first_agent/agent.py
@@ -8,7 +8,7 @@ from google.adk.agents.llm_agent import Agent
 # )
 
 root_agent = Agent(
-    model='gemini-3.5-flash',
+    model='gemini-2.5-flash',
     name='math_tutor_agent',
     description='Helps students learn algebra by guiding them through problemsolving steps',
     instruction='You are a patient math tutor. Help students with algebra problems.'
